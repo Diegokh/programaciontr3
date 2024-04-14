15686ace9370894cd4b396d1040fe2d7a7cd61e7
Diegokh <diegoblanque04@outlook.com>
2024-04-14 12:17:26 +0200

    nuevos cambios en el apartado c del ejercicio 1

--- src/programaciontr3/Programaciontr3.java
+++ src/programaciontr3/Programaciontr3.java
@@ -21,7 +21,7 @@ public class Programaciontr3 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         
       /* //Ruta del archivo
         String rutaArchivo = "E:\\MisTareas\\Archivo_de_texto_Etica_en_el_ID_Digital.txt";
@@ -36,21 +36,38 @@ public class Programaciontr3 {
         
         try {
             // Ruta del archivo
-            archivo = new File("E:\\MisTareas\\Archivo de texto Etica en el ID Digital.txt");
+            archivo = new File("F:\\MisTareas\\Archivo de texto Etica en el ID Digital.txt");
             
             // Creo los flujos
             if (archivo.exists()) {
                 flujo1 = new FileInputStream(archivo);
                 disflujo1 = new DataInputStream(flujo1);
+                System.out.println("El archivo existe ");
+                
+                System.out.println("Ejercicio B: Salto 50 bytes hacia delante y lee los 50 siguientes");
+                disflujo1.skipBytes(50);
+                byte[] arrayBytes = new byte[50];
+                disflujo1.read(arrayBytes);
+                for(byte b : arrayBytes){
+                    System.out.println(b+"");
+                }
+                System.out.println("Ejercicio C: Lee 15 cadenas de caracteres");
+                System.out.println("Leo 15 cadenas de caracteres siguientes:");
+                for(int i=0; i < 15; i++){
+                    String cadena = disflujo1.readUTF();
+                    System.out.println(cadena);
+                }
+                
                 
              
                 
-                System.out.println("El archivo existe ");
+                
             } else {
                 throw new FileNotFoundException("No existe el archivo " + archivo.getName());
             }
             }catch(FileNotFoundException e){
                 System.out.println("No se pudo encontrar el archivo"+e.getMessage());
             }
+        
     
     }}
